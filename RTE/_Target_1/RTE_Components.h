df0949d223c4d64bafe568ca059efb13118aa372
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2020-04-21 13:20:55 +0300

    Убрал лишние компоненты.

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -18,10 +18,6 @@
 
 /*  Keil::Device:StdPeriph Drivers:Framework:3.5.1 */
 #define RTE_DEVICE_STDPERIPH_FRAMEWORK
-/*  Keil::Device:StdPeriph Drivers:GPIO:3.5.0 */
-#define RTE_DEVICE_STDPERIPH_GPIO
-/*  Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
-#define RTE_DEVICE_STDPERIPH_RCC
 
 
 #endif /* RTE_COMPONENTS_H */
--- main.c
+++ main.c
@@ -1,6 +1,4 @@
 #include "stm32f10x.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_rcc.h"
 #include "initialization.h"
 #include "delay.h"
 
@@ -14,26 +12,26 @@ int main(void)
 	
 	for(;;)
 	{
-		GPIOB->ODR |= GPIO_Pin_9;
-		GPIOA->ODR |= GPIO_Pin_1;
+		GPIOB->ODR |= GPIO_ODR_ODR9;
+		GPIOA->ODR |= GPIO_ODR_ODR1;
 		
 		if(a1_t < b9_t)
 		{
 			delay(a1_t);
-			GPIOA->ODR &= ~GPIO_Pin_1;
+			GPIOA->ODR &= ~GPIO_ODR_ODR1;
 			
 			delay(b9_t - a1_t);
-			GPIOB->ODR &= ~GPIO_Pin_9;
+			GPIOB->ODR &= ~GPIO_ODR_ODR9;
 		
 			delay(T - b9_t);
 		}
 		else
 		{
 			delay(b9_t);
-			GPIOB->ODR &= ~GPIO_Pin_9;
+			GPIOB->ODR &= ~GPIO_ODR_ODR9;
 			
 			delay(a1_t - b9_t);
-			GPIOA->ODR &= ~GPIO_Pin_1;
+			GPIOA->ODR &= ~GPIO_ODR_ODR1;
 		
 			delay(T - a1_t);
 		}
